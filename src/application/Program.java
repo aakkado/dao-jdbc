eb2d8b096fea4879e91f6d79d6f644c5fa348a9c
Matheus Lopes <m0039439@ems.com.br>
2023-07-28 14:17:37 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,16 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
         System.out.println("--------------------------------------------");
+        System.out.println();
+
+        System.out.println("----- Test #5: seller update -----");
+        seller = sellerDao.findById(1);
+        seller.setName("Marta Wayne");
+        seller.setEmail("marta@gmail.com");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+        System.out.println("--------------------------------------------");
+
     }
 }
 
